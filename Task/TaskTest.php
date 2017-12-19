b531ffa43a83325c46e160a16ca6fbd9f1fb8fbd
Yzy <vs123kkom@qq.com>
2017-12-19 20:15:14 +0800

    Create TaskTest.php

---
+++ Task/TaskTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Task;
+use ZeroCost\Core\Task;
+class TaskTest implements Task{
+    function run(int $page): bool
+    {
+        // TODO: Implement run() method.
+    }
+    function isException(\Exception $e)
+    {
+        // TODO: Implement isException() method.
+    }
+}
