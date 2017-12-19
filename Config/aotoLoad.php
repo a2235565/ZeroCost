6ad8143c01ada3d0da84d5892c70171d1a1e8b8a
Yzy <vs123kkom@qq.com>
2017-12-19 23:06:04 +0800

    Update aotoLoad.php

--- Config/aotoLoad.php
+++ Config/aotoLoad.php
@@ -6,5 +6,5 @@ return array(
     'Core/Register'=>true,//注册树
     'Core/Error'=>true,//注册树
     'Core/Task_Php5'=>true,//webTask
-    'Core/Task_Php7'=>true,//webTask
+     //'Core/Task_Php7'=>true,//webTask php7
 );
