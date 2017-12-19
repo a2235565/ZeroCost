fcdbe92f0e59d3094c10dfac61a5da67d1dae7d8
Yzy <vs123kkom@qq.com>
2017-12-19 23:19:51 +0800

    Update TaskTest5.php

--- Task/TaskTest5.php
+++ Task/TaskTest5.php
@@ -5,10 +5,16 @@ use ZeroCost\Core\Task_Php5;
 class TaskTest5 implements Task_Php5 {
     function run($page)
     {
+        echo $page;
+        if($page==100){
+            return false;
+        }
+        return true;
         // TODO: Implement run() method.
     }
     function isException(\Exception $e)
     {
+        echo 'ok';
         // TODO: Implement isException() method.
     }
     function go(){
