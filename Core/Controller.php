51f70f9c53f4482925090c5cafbb066d91abd87b
Yzy <vs123kkom@qq.com>
2017-12-19 17:36:20 +0800

    舍弃Controller层DB功能

--- Core/Controller.php
+++ Core/Controller.php
@@ -6,10 +6,24 @@
  * Time: 23:47
  */
 namespace Core;
-use ZeroCost\Core\Register;
+//use ZeroCost\Core\Register;
 class Controller{
-    protected $db;
-    function __construct(){
-      $this->db  =  Register::get('db');
+   //protected $db;
+    function __construct()
+    {
+        header("Content-Type: text/html; charset=utf8");
+        //$this->db = Register::get('db');
+    }
+
+    function  display($file)
+    {
+        $file = ROOTPATH . "/View/{$file}.html";
+        if (is_file($file)) {
+            include $file;
+//            $str = file_get_contents($file);
+//            echo($str);
+        } else {
+            trigger_error('找不到模板' . $file, E_USER_ERROR);
+        }
     }
 }
