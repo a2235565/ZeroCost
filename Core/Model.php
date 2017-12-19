4660717ccd2e08f5e2e262642ef948e7556501b8
Yzy <vs123kkom@qq.com>
2017-12-19 17:48:02 +0800

    Update Model.php

--- Core/Model.php
+++ Core/Model.php
@@ -4,16 +4,9 @@ class Model
 {
     protected $db = null;
     function __construct(){
-        $conf = include ROOTPATH . '/Config/conf.php';
-        if($conf['debug']){
-            //开发模式方便IDE提示
-            $this->db =  new \Db\medoo(include ROOTPATH . '/Config/dbConf.php');
-        }else{
-            //部署模式使用
-            $this->db =  \ZeroCost\Core\Register::get('db');
-        }
+        //部署模式使用
+        $this->db =  \ZeroCost\Core\Register::get('db');
     }
-
     /**
      * ide 方法提示
      * @return \Db\medoo
