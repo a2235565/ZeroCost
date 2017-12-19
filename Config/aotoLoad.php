342ed1dea2475093a491edab5ae16f5fcd9c780c
Yzy <vs123kkom@qq.com>
2017-12-19 22:59:39 +0800

    Update aotoLoad.php

--- Config/aotoLoad.php
+++ Config/aotoLoad.php
@@ -5,4 +5,6 @@ return array(
     'Core/Autoload'=>true,//自动载入类
     'Core/Register'=>true,//注册树
     'Core/Error'=>true,//注册树
+    'Core/Task_Php5'=>true,//webTask
+    'Core/Task_Php7'=>true,//webTask
 );
