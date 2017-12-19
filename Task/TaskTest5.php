175840945261afa0214f07b67ebeb22e24517adc
Yzy <vs123kkom@qq.com>
2017-12-19 22:47:01 +0800

    Update TaskTest5.php

--- Task/TaskTest5.php
+++ Task/TaskTest5.php
@@ -1,16 +1,18 @@
 <?php
-namespace ZeroCost\Core;
-interface  Task_Php5
-{
-    /**
-     * @param $page 当前任务页码
-     * @return true: 当前执行成功 false ：任务结束
-     */
-    function run($page)  ;
+namespace Task;
+use ZeroCost\Core\Task_Php5;
 
-    /**
-     * 异常处理
-     * @return mixed
-     */
-    function isException(\Exception $e);
+class TaskTest5 implements Task_Php5 {
+    function run($page)
+    {
+        // TODO: Implement run() method.
+    }
+    function isException(\Exception $e)
+    {
+        // TODO: Implement isException() method.
+    }
+    function go(){
+        //运行入口
+        taskForWebRun('\Task\TaskTest5',100);
+    }
 }
